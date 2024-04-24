82b963bbece4aada5c924fc1621d05359e4c3eef
olivemochi3 <jeryfish218@gmail.com>
2024-04-24 17:27:03 +0900

    third commit dash

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -2,7 +2,11 @@ import Section from "./section";
 
 export default function Contact() {
   return (
-    <Section title="お問合せ" subTitle="気軽にご連絡ください">
+    <Section
+      title="お問合せ"
+      subTitle="気軽にご連絡ください"
+      className="bg-gray-100" // 背景色を追加
+    >
       <div className="flex flex-col items-center justify-center text-center">
         <p className="mb-2">電話番号：000-0000-0000</p>
         <p>メールアドレス：example@example.com</p>
--- app/components/section.tsx
+++ app/components/section.tsx
@@ -1,14 +1,16 @@
 export default function Section({
-    title,
-    subTitle,
-    children,
+  title,
+  subTitle,
+  children,
+  className = '', // デフォルト値を空文字列に設定
 }: {
-    title: string
-    subTitle: string
-    children: React.ReactNode
+  title: string
+  subTitle: string
+  children: React.ReactNode
+  className?: string // オプショナルなプロパティとして追加
 }) {
-  return (
-    <section className="py-20 container">
+return (
+  <section className={`py-20 container ${className}`}>
     <div className="text-center space-y-6 mb-10">
       <h2 className="font-bold text-2xl">{title}</h2>
       <p className="text-muted-forground">{subTitle}</p>
@@ -16,5 +18,5 @@ export default function Section({
 
     {children}
   </section>
-  )
+)
 }

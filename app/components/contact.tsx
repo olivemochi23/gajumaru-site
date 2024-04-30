2b75249204620ed6286dd73313fbf4207fb5c37a
olivemochi3 <jeryfish218@gmail.com>
2024-04-30 19:50:44 +0900

    contact button

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -4,8 +4,8 @@ import { PhoneCall } from "lucide-react"
 
 export function ButtonWithIcon() {
   return (
-    <div className="relative z-10">
-      <Button className=" text-white rounded-full hover:bg-opacity-80 active:bg-opacity-100 transition-opacity duration-300">
+    <div className="relative z-10 flex justify-center items-center">
+      <Button className="bg-orange-500 active:bg-orange-500 z-10 text-white rounded-full ">
         <PhoneCall className="mr-2 h-4 w-4" /> 000-0000-0000
       </Button>
     </div>
@@ -18,9 +18,7 @@ export default function Contact() {
       title="お問合せ"
       subTitle="お問合せのご連絡はお電話にて受け付けております。ご不明点等ございましたらお気軽にお問合せください。"
       className="relative px-4 sm:px-6 lg:px-8 flex flex-col justify-center items-center bg-center bg-no-repeat bg-cover"
-      style={{
-        ///backgroundImage: "url('/お問合せ背景.png')",
-      }}
+      style={{}}
     >
       <ButtonWithIcon />
     </Section>

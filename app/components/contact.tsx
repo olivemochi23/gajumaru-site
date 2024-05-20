46987b6bc2c5edb19830ca6b5a1ef9686a67bac2
olivemochi23 <magnis.corporation@gmail.com>
2024-05-20 16:14:03 +0900

    chore: Remove unused code and fix missing closing tag in contact component

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -20,7 +20,7 @@ export default function Contact() {
       className="relative px-4 sm:px-6 lg:px-8 flex flex-col justify-center items-center bg-center bg-no-repeat bg-cover"
       style={{}}
     >
-      <ButtonWithIcon />
+
     </Section>
   );
 }

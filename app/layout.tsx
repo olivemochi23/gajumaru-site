de28b9f401c8619ae6be77b7428e6064225d5246
olivemochi23 <magnis.corporation@gmail.com>
2024-05-21 11:31:41 +0900

    change meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,14 +11,14 @@ const zenKakuGothicNew = Zen_Kaku_Gothic_New({
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://gajumaru-site.vercel.app/'), // ここにあなたのドメインを設定します
-  title: "株式会社ガジュマル - 海の家、レンタカー、パンケーキ、マリンジェットレンタル",
-  description: "小豆島で海の家、レンタカー、パンケーキ、マリンジェットレンタルを提供する観光会社。小豆島での最高の体験をご提供します。",
+  title: "株式会社ガジュマル - 海の家、レンタカー、ジェットスキーレンタル",
+  description: "小豆島で最高のリゾート体験を提供する観光事業を行っている会社です。",
   icons: {
     icon: "/favicon.ico",
   },
   openGraph: {
     title: "株式会社ガジュマル",
-    description: "小豆島で海の家、レンタカー、パンケーキ、マリンジェットレンタルを提供する観光会社です。",
+    description: "小豆島で最高のリゾート体験を提供する観光事業を行っている会社です。",
     images: "/og-image.jpg",
     type: "website",
   },

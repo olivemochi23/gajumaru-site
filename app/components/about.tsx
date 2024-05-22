ad0efb7d61e79103b432366b71a208454f9b3915
olivemochi23 <magnis.corporation@gmail.com>
2024-05-23 02:27:54 +0900

    change about

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -13,7 +13,7 @@ export default function About() {
           
             }}
       >
-        <div className=" gap-4 ">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
           <div className="  py-20">
             <h4 className=" text-xl font-bold">上質な施設</h4>
             <p>

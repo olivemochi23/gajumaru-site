44d13e98421458f07d6c843f88b829c1bb6c7ffb
olivemochi23 <magnis.corporation@gmail.com>
2024-05-27 16:43:46 +0900

    change maps

--- app/components/accsess.tsx
+++ app/components/accsess.tsx
@@ -1,5 +1,5 @@
-import { GoogleMapsEmbed } from '@next/third-parties/google'
-import Section from '@/app/components/section'
+import React from 'react';
+import Section from '@/app/components/section';
 
 export default function Access() {
   return (
@@ -18,16 +18,17 @@ export default function Access() {
           whiteSpace: 'pre-wrap',
         }}
       >
-        <div className="">
-          <GoogleMapsEmbed
-            apiKey="AIzaSyDf5vxFKOvYRh1pywfhlbyoGBrAu1Rnd3U"
-            height={400}
-            width={400}
-            mode="place"
-            q="香川県小豆郡小豆島町西村甲1490-1"
+        <div className="w-full h-96">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d822.3087636317799!2d134.28012872856186!3d34.47156089830564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355479004e74d17f%3A0xe455c7a42f07c8f2!2z44Ks44K444Ol44Oe44Or44OG44Op44K5!5e0!3m2!1sja!2sjp!4v1716795532227!5m2!1sja!2sjp"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            allowFullScreen
+            aria-hidden="false"
           />
         </div>
       </Section>
     </div>
-  )
+  );
 }

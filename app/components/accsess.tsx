297b188119ef808fae3f51c617a8294c45bb2f3a
olivemochi23 <magnis.corporation@gmail.com>
2024-05-28 11:53:25 +0900

    change maps position

--- app/components/accsess.tsx
+++ app/components/accsess.tsx
@@ -14,7 +14,7 @@ export default function Access() {
       }
       className="px-4 sm:px-6 lg:px-8"
     >
-      <div className="w-full h-96">
+      <div className="w-full h-96 flex justify-center items-center">
         <iframe 
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13156.940217841357!2d134.2807738!3d34.4715609!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355479004e74d17f%3A0xe455c7a42f07c8f2!2z44Ks44K444Ol44Oe44Or44OG44Op44K5!5e0!3m2!1sja!2sjp!4v1716864241649!5m2!1sja!2sjp" 
           width={400}

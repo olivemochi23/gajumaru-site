cc3d649c27d7043ccdbb4ba89d4b5223cd085639
olivemochi23 <magnis.corporation@gmail.com>
2024-07-10 11:04:07 +0900

    fix hero jpg

--- app/renta-car/components/Hero.tsx
+++ app/renta-car/components/Hero.tsx
@@ -4,14 +4,14 @@ const Hero = () => {
   return (
     <div className="relative">
       <div className="relative w-full h-96">
-        <Image src="/maincar.jpg" alt="小豆島のイラスト" layout="fill" objectFit="cover" />
+        <Image src="/rentacar-hero.jpg" alt="小豆島の風景" layout="fill" objectFit="cover" />
       </div>
 
       <div className="absolute inset-0 flex items-center justify-center ">
         <div className="text-center p-4 rounded-lg">
-        <h2 className="text-2xl font-bold text-pink-500 text-shadow-white-outline">
+        <h2 className="text-lg font-bold text-pink-500 text-shadow-white-outline">
           小豆島の観光体験をもっと快適に！<br />
-          どこでも借りられるガジュマルレンタカー！
+          ワンランク上の旅を楽しむなら小豆島ガジュマルレンタカーで決まり！
         </h2>
         </div>
       </div>

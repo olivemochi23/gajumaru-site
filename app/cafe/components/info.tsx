3814214587602d046113939bb64baf9b6dc7255d
olivemochi23 <magnis.corporation@gmail.com>
2024-07-10 16:06:56 +0900

    fix

--- app/cafe/components/info.tsx
+++ app/cafe/components/info.tsx
@@ -9,7 +9,7 @@ export default function Info() {
       className="text-pink-800"
     >
     <div className="mt-6 space-y-4 text-gray-600">
-      <p className="flex items-center"><span className="material-icons mr-2"></span>営業時間: 10:00 - 16:00（L.O. 変動あり）</p>
+      <p className="flex items-center"><span className="material-icons mr-2"></span>営業時間: 10:00 - 16:00 <br />※L.O. 変動あり</p>
       <p className="flex items-center"><span className="material-icons mr-2"></span>定休日: 不定休</p>
       <p className="flex items-center"><span className="material-icons mr-2"></span>座席数: 20席（テラス席）</p>
     </div>

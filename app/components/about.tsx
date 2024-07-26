4d8ae4148d39f13be8501613e0ef4e8e165ec39d
olivemochi23 <magnis.corporation@gmail.com>
2024-07-26 17:01:48 +0900

    fix

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -13,26 +13,26 @@ export default function About() {
           
             }}
       >
-        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
-          <div className="  py-20">
-            <h4 className=" text-xl font-bold">上質な施設</h4>
-            <p>
-              地中海の雰囲気に包まれた上質な施設で、リラックスした時間をお過ごしいただけます。
-            </p>
-          </div>
-          <div className=" py-20">
-            <h4 className=" text-xl font-bold">きめ細かなサービス</h4>
-            <p>
-              お客様一人ひとりのニーズに合わせた、きめ細かなサービスをご提供します。
-            </p>
-          </div>
-          <div className=" py-20">
-            <h4 className=" text-xl font-bold">豊かな自然</h4>
-            <p>
-              小豆島の豊かな自然に囲まれた環境で、心身ともにリフレッシュできます。
-            </p>
-          </div>
-        </div>
+       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+  <div className="py-20 bg-green-100 rounded-3xl">
+    <h4 className="text-xl font-bold text-center mb-2">上質な施設</h4>
+    <p className="px-4">
+      地中海の雰囲気に包まれた上質な施設で、リラックスした時間をお過ごしいただけます。
+    </p>
+  </div>
+  <div className="py-20 bg-green-100 rounded-3xl">
+    <h4 className="text-xl font-bold text-center mb-2">きめ細かなサービス</h4>
+    <p className="px-4">
+      お客様一人ひとりのニーズに合わせた、きめ細かなサービスをご提供します。
+    </p>
+  </div>
+  <div className="py-20 bg-green-100 rounded-3xl">
+    <h4 className="text-xl font-bold text-center mb-2">豊かな自然</h4>
+    <p className="px-4">
+      小豆島の豊かな自然に囲まれた環境で、心身ともにリフレッシュできます。
+    </p>
+  </div>
+</div>
       </Section>
     </div>
   )
